1d1673dd4483fd06790140a245871f605b55b012
Averta4 <martagge@mail.ru>
2026-05-02 10:53:34 +0300

    Delete fri directory

--- fri/src/main/java/repository/Userr.java
+++
@@ -1,7 +0,0 @@
-package com.example.fri.repository;
-
-import com.example.fri.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Userr extends JpaRepository<User, Long> {
-}
